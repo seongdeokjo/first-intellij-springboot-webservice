fdf9c68f627e345f12dcfdf7463bceba521ba9fd
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-10-15 16:02:11 +0900

    feat: update server file

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,7 +3,7 @@
 # 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 
 function find_idle_profile() {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # 현재 엔진엑스가 바라보고 있는 스프링 부트가 정상적으로 수행 중인지 확인
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1/profile) # 현재 엔진엑스가 바라보고 있는 스프링 부트가 정상적으로 수행 중인지 확인
                                                           # 응답값을 HttpStatus로 받음
                                                           # 정상이면 200, 오류가 발생한다면 400~503 사이로 발생 400 이상은 모두 예외로 보고 real2를 현재 profile로 사용
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면(40x,50x 에러 모두 포함)
@@ -11,7 +11,7 @@ function find_idle_profile() {
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s http://127.0.0.1/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,7 +11,7 @@ function switch_proxy(){
   echo "> Port 전환"
   # 하나의 문장을 만들어 파이프라인(|)으로 넘겨주기 위해 echo 사용
   # 엔진엑스가 변경할 프록시 주소를 생성 쌍따옴표를 사용, 만약 사용하지 않으면 $service_url을 그대로 인식하지 못하고 변수를 찾게됨
-  echo "set \$service_url http://127.0.0.1${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
                                                             # tee -> 앞에서 넘겨준 문자을 service_url.inc에 덮어씀
 
   echo "> 엔진엔스 Reload"
